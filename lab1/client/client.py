b658fb439d7274b82bb4f3a18a83ff4ee01d9734
WeberLu <43847259+weberlu88@users.noreply.github.com>
2023-03-05 17:48:25 +0800

    lab1 client connect ok

--- lab1/client/client.py
+++ lab1/client/client.py
@@ -1,3 +1,6 @@
 import requests
 
 requests.get('http://server:5000')
+
+# add success log by my self
+print('client connect to server successfully. exit.')
--- lab1/server/server.py
+++ lab1/server/server.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def get():
-    # with open('data/msg.txt', 'w') as f:
-    #     f.write("Hi!\n")
+    with open('data/msg.txt', 'w') as f:
+        f.write("Hi!\n")
+    print("server get your request.")
     return "Hi!"
 
 app.run()
